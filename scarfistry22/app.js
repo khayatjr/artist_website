c633ea5fb34975b7c07a650321941be1b2d5eb1d
khayatjr <khayatove@yahoo.com>
2020-04-05 18:07:02 +0200

    Update app.js

--- scarfistry22/app.js
+++ scarfistry22/app.js
@@ -24,7 +24,7 @@ var bodyparser= require("body-parser");
 
 // mongodb://localhost:27017/scarfistry
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://Youssef:q6lq677vzble1CNq@cluster0-ekeib.mongodb.net/test?retryWrites=true&w=majority";
+const uri = "mongodb+srv://Youscdhjkf:q6lq677vzble1CNq@cluster0-ekeib.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
   console.log("hey");
