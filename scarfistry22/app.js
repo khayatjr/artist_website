9b0b9e9d33a4a4b41460ac705a4205fed4b4a348
khayatjr <khayatove@yahoo.com>
2020-04-05 15:56:46 +0200

    Update app.js

--- scarfistry22/app.js
+++ scarfistry22/app.js
@@ -25,7 +25,15 @@ var bodyparser= require("body-parser");
 // mongodb://localhost:27017/scarfistry
 
 
-mongoose.connect('mongodb+srv://Youssef:1234@cluster0-ekeib.mongodb.net/test?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://Youssef:1234@cluster0-ekeib.mongodb.net/test?retryWrites=true&w=majority',{
+	useNewUrlParser:true,
+	useCreateIndex:true
+
+	}).then(() =>{
+		console.log('DB connected');
+	}).catch (err =>{
+		console.log('Error:',err.message);
+	});
 // const UserSchema = new mongoose.Schema({
 //     name :String
         
