4e61521c540888efd9600243476f58dd87d06088
khayatjr <khayatove@yahoo.com>
2020-04-05 15:17:03 +0200

    Update app.js

--- scarfistry22/app.js
+++ scarfistry22/app.js
@@ -25,10 +25,10 @@ var bodyparser= require("body-parser");
 // mongodb://localhost:27017/scarfistry
 
 
-// mongoose.connect('mongodb+srv://Youssef:<1234>@cluster0-ekeib.mongodb.net/test?retryWrites=true&w=majorityry',{
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true 
-// })
+mongoose.connect('mongodb+srv://Youssef:<1234>@cluster0-ekeib.mongodb.net/test?retryWrites=true&w=majorityry',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+})
 // const UserSchema = new mongoose.Schema({
 //     name :String
         
